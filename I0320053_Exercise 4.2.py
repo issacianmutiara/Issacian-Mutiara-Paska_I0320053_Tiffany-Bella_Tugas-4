544896246a9391a75333a455192fd65f4fc68029
I Mutiara <issacianmutiara@gmail.com>
2021-03-22 20:45:04 +0700

    I0320053_Exercise 4.2

---
+++ I0320053_Exercise 4.2.py
@@ -0,0 +1,24 @@
+#Operator Relasional
+
+x = 5
+x = 8
+y = 10
+
+#Output: x > y is true
+print('x > y =' ,x > y)
+
+#Output: x < y is true
+print('x < y =' ,x < y)
+
+Output: x == y is False
+print('x == y=' ,x ==y)
+
+#Output: x != y is true
+print('x != y=' ,x != y)
+
+#Output: x >= y is False
+print('x >= y=' ,x >= y)
+
+#Output: x <= y is True
+print('x <= y=' ,x <= y)
+
