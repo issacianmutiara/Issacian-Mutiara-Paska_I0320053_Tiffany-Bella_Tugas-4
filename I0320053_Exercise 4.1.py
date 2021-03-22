0c6d877a9d6a5f6405e226633c2ff05e0c006e57
I Mutiara <issacianmutiara@gmail.com>
2021-03-22 20:22:02 +0700

    I0320053_Exercise 4.1

---
+++ I0320053_Exercise 4.1.py
@@ -0,0 +1,22 @@
+#Operator Aritmatika
+
+x = 18
+y = 7
+
+#Output: x + y = 25
+print('x + y =' ,x+y)
+
+#Output: x - y = 11
+print('x-y=' ,x-y)
+
+#Output: x * y = 126
+print('x*y=' ,x*y)
+
+#Output: x / y = 2,57
+print('x / y=' ,x / y)
+
+#Output: x // y = 2
+print('x // y=' ,x // y)
+
+#Output: x ** y = 612220032
+print('x**y=' ,x**y)
